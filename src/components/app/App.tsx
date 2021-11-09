4bbbc539c53865b21a479f78452eb595b52e3b59
FFiillaattoovv <admirator2015@yandex.ru>
2021-11-09 15:23:54 +0300

    Add Spinner and refactoring

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,7 +6,7 @@ import "./App.css"
 
 export const App = () => {
     return (
-        <div  className="stardb-app">
+        <div className="stardb-app">
             <Header/>
             <RandomPlanet/>
             <div className="row mb2">
--- src/components/random-planet/RandomPlanet.tsx
+++ src/components/random-planet/RandomPlanet.tsx
@@ -1,6 +1,7 @@
 import "./RandomPlanet.css"
-import {Component} from "react";
+import {Component, FC} from "react";
 import {SwapiService, TransformPlanetType} from "../../services/swapiService";
+import {Spinner} from "../spinner/Spinner";
 
 export class RandomPlanet extends Component<{}, RandomPlanetStateType> {
 
@@ -16,11 +17,12 @@ export class RandomPlanet extends Component<{}, RandomPlanetStateType> {
         name: null,
         population: null,
         rotationPeriod: null,
-        diameter: null
+        diameter: null,
+        loading: true
     }
 
     onPlanetLoaded = (planet: TransformPlanetType) => {
-        this.setState({...planet})
+        this.setState({...planet, loading: false})
     }
 
     updatePlanet() {
@@ -32,39 +34,60 @@ export class RandomPlanet extends Component<{}, RandomPlanetStateType> {
 
     render() {
 
-        const {id, name, population, rotationPeriod, diameter} = this.state
+        const {id, name, population, rotationPeriod, diameter, loading} = this.state
+
+        const spinner = loading ? <Spinner/> : null
+        const content = !loading ? <PlanetView id={id} name={name} population={population} rotationPeriod={rotationPeriod}
+                        diameter={diameter}/> : null
 
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt=""/>
-                <div>
-                    <h4>{name}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {spinner}
+                {content}
             </div>
         )
     }
 }
 
+const PlanetView: FC<PlanetViewPropsType> = ({id, name, population, rotationPeriod, diameter}) => {
+    return (
+        <>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt=""/>
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
 // types
 type RandomPlanetStateType = {
-    id:  null | string,
-    name: null | string,
-    population: null | string,
-    rotationPeriod: null | string,
+    id: null | string
+    name: null | string
+    population: null | string
+    rotationPeriod: null | string
+    diameter: null | string
+    loading: boolean
+}
+type PlanetViewPropsType = {
+    id: null | string
+    name: null | string
+    population: null | string
+    rotationPeriod: null | string
     diameter: null | string
 }
